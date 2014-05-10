09641490e9a9cce4161fc57f9ae4e330226fa0eb
PeterGok <petergok@hotmail.com>
2014-05-10 19:40:58 -0400

    Created a simple django web server that opens an html file

--- view_ranch_website/settings.py
+++ view_ranch_website/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -98,3 +99,5 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, "templates")]
